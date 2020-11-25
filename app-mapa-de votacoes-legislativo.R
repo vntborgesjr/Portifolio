2cd45f83fb2ac826c967e95e13c6f6459436f3d7
Vitor Borges Jr <vntborgesjr@gmail.com>
2020-11-25 19:33:03 -0300

    Update app-mapa-de votacoes-legislativo.R

--- app-mapa-de votacoes-legislativo.R
+++ app-mapa-de votacoes-legislativo.R
@@ -11,14 +11,13 @@ library(tmap)
 library(shiny)
 
 # Carregar dados  -------------------------------------------
-cham_18 <- read_csv("datasets/cham_18.csv")[, -1]
 cham_19 <- read_csv("datasets/cham_19.csv")[, -1]
 cham_20 <- read_csv("datasets/cham_20.csv")[, -1]
 regiao_shp <- readOGR(dsn = "regioes_2010", layer = "regioes_2010")
 estados_shp <- readOGR(dsn = "estados_2010", layer = "estados_2010")
 
 # Agrupar dados das votações  -------------------------------------------
-legislativo <- rbind(cham_18, cham_19, cham_20)
+legislativo <- rbind(cham_19, cham_20)
 
 # sumarizar dados ---------------------------------------------------------
 # Resultado por matéria
@@ -29,23 +28,25 @@ resultado_materia <- legislativo %>%
                               ifelse(legislator_vote == "Art. 17", 0, 0)))) %>% 
   group_by(rollcall_id, Votos = legislator_vote, ano) %>% 
   filter(legislator_vote %in% c("Sim", "Nao")) %>% 
-  summarise(vote = sum(vote))
+  summarise(vote = sum(vote)) %>% 
+  ungroup() %>% 
+  select(materia = rollcall_id, Votos, total = vote)
 
 # estado
-estado_votos <- legislativo %>% 
-  group_by(rollcall_id, legislator_state, ano) %>% 
-  mutate(total = ifelse(legislator_vote == "Sim", 1, 
-                        ifelse(legislator_vote == "Nao", -1, 
-                               ifelse(legislator_vote == "Art. 17", 0, 0))),
-         sim = ifelse(legislator_vote == "Sim", 1, 
-                      ifelse(legislator_vote == "Nao", 0, 
-                             ifelse(legislator_vote == "Art. 17", 0, 0))),
-         nao = ifelse(legislator_vote == "Sim", 0, 
-                      ifelse(legislator_vote == "Nao", -1, 
-                             ifelse(legislator_vote == "Art. 17", 0, 0)))) %>% 
-  group_by(rollcall_id, legislator_state, ano) %>% 
-  filter(legislator_vote %in% c("Sim", "Nao")) %>% 
-  summarise(saldo = sum(total), sim = sum(sim), nao = sum(nao))
+#estado_votos <- legislativo %>% 
+ # group_by(rollcall_id, legislator_state, ano) %>% 
+  #mutate(total = ifelse(legislator_vote == "Sim", 1, 
+   #                     ifelse(legislator_vote == "Nao", -1, 
+    #                           ifelse(legislator_vote == "Art. 17", 0, 0))),
+     #    sim = ifelse(legislator_vote == "Sim", 1, 
+      #                ifelse(legislator_vote == "Nao", 0, 
+       #                      ifelse(legislator_vote == "Art. 17", 0, 0))),
+        # nao = ifelse(legislator_vote == "Sim", 0, 
+         #             ifelse(legislator_vote == "Nao", -1, 
+          #                   ifelse(legislator_vote == "Art. 17", 0, 0)))) %>% 
+  #group_by(rollcall_id, legislator_state, ano) %>% 
+  #filter(legislator_vote %in% c("Sim", "Nao")) %>% 
+  #summarise(saldo = sum(total), sim = sum(sim), nao = sum(nao))
 
 # partido
 partido_votos <- legislativo %>% 
@@ -61,49 +62,22 @@ partido_votos <- legislativo %>%
                              ifelse(legislator_vote == "Art. 17", 0, 0)))) %>% 
   group_by(rollcall_id, legislator_state, legislator_party, legislator_vote, ano) %>% 
   filter(legislator_vote %in% c("Sim", "Nao")) %>% 
-  summarise(saldo = sum(saldo), sim = sum(sim), nao = sum(nao))
+  summarise(saldo = sum(saldo), sim = sum(sim), nao = sum(nao)) %>%
+  ungroup() %>% 
+  select(ano, materia = rollcall_id, sigla = legislator_state, 
+         partido = legislator_party, nao, sim, saldo)
 
 # Gerar objetos espaciais para as diferentes camadas  -------------------------------------------
-camada_estado <- merge(x = estados_shp, y = estado_votos,
-                       by.x = "sigla", by.y = "legislator_state", 
-                       duplicateGeoms = TRUE) # to merge a spatial object to a data frame
+estados_shp@data <- data.frame(estados_shp@data[, 3])
+names(estados_shp@data) <- "sigla"
 
-camada_partido <- merge(x = estados_shp, y = partido_votos,
-                        by.x = "sigla", by.y = "legislator_state", 
+camadas_partidos <- merge(x = estados_shp, y = partido_votos,
+                        by = "sigla", 
                         duplicateGeoms = TRUE) # to merge a spatial object to a data frame
 
-# gerando camadas saldo, sim e não de mapa por estado
-camada_estado <- camada_estado[, c("rollcall_id", "sigla",
-                                   "nao", "sim", "saldo")]
-estado <- list()
-
-for (i in 3:5) {
-  estado[[i]] <- camada_estado[, 
-                               c(1, 2, i)]
-}
-
-estado <- c(nao = estado[[3]], sim = estado[[4]], saldo = estado[[5]])
-head(estado$nao)
-# gerando camadas saldo, sim e não de mapa por partido
-index <- length(levels(factor(camada_partido@data$legislator_party)))
-partidos_nao <- list()
-partidos_sim <- list()
-partidos_saldo <- list()
-
-for (i in 1:index){
-  # votos nao por partido por estado
-  partidos_nao[[i]] <- camada_partido[camada_partido$legislator_party == levels(factor(camada_partido$legislator_party))[i],
-                                      c("rollcall_id", "sigla", "legislator_party", "nao")]
-  names(partidos_nao)[[i]] <- levels(factor(camada_partido@data$legislator_party))[i]
-  # votos sim por partido por estado
-  partidos_sim[[i]] <- camada_partido[camada_partido$legislator_party == levels(factor(camada_partido$legislator_party))[i],
-                                      c("rollcall_id", "sigla", "legislator_party", "sim")]
-  names(partidos_sim)[[i]] <- levels(factor(camada_partido@data$legislator_party))[i]
-  # saldo da votação por partido por estado
-  partidos_saldo[[i]] <- camada_partido[camada_partido$legislator_party == levels(factor(camada_partido$legislator_party))[i],
-                                        c("rollcall_id", "sigla", "legislator_party", "nao", "sim", "saldo")]
-  names(partidos_saldo)[[i]] <- levels(factor(camada_partido@data$legislator_party))[i]
-}
+camadas <- merge(x = camadas_partidos, y = resultado_materia,
+                 by = "materia", 
+                 duplicateGeoms = TRUE) # to merge a spatial object to a data frame
 
 # Construir a interface do usuário  -------------------------------------------
 iu <- fluidPage(
@@ -114,18 +88,15 @@ iu <- fluidPage(
     sidebarPanel(
       # seletor para o ano
       sliderInput(inputId = 'ano', label = 'Escolha um ano: ', 
-                  min = 2018, max = 2020, value = 2020),
+                  min = 2019, max = 2020, value = 2020),
       # seletor de temas
       
       # seletor de matéria
-      selectInput('rollcall_id', 'Selecione a matéria: ', 
-                  choices = camada_estado$rollcall_id),
+      selectInput('materia', 'Selecione a matéria: ', 
+                  choices = camadas$materia),
       # seletor de partido
-      selectInput("legislator_party", "Selecione um partido: ",
-                  choices = camada_partido$legislator_party)
-      # seletor de votação
-      #selectInput('nao', 'Selecione o resultado: ', 
-      #            choices = c('nao', 'sim', 'saldo'), selected = 'saldo'),
+      selectInput("partido", "Selecione um partido: ",
+                  choices = camadas$partido)
     ),
       # painel principal para o mapa
       mainPanel(
@@ -146,22 +117,7 @@ iu <- fluidPage(
 servidor <- function(input, output, session) {
   # Função para selecionar os dados por estado - precisa selecionar 
   # os dados primeiro senão vai dar errado
-  plot_resultado_materia <- function() {
-    resultado_materia %>% 
-      filter(ano == 2020) %>% 
-      filter(rollcall_id ==  "MPV-897-2019-1") %>% 
-      ggplot(aes(x = "", y = vote, fill = Votos)) +
-      geom_bar(colour = "black", stat = "identity", width = 1) +
-      coord_polar("y", start = 0) +
-      theme_minimal() + 
-      theme(line = element_blank(),
-            axis.title.x = element_blank(),
-            axis.title.y = element_blank(),
-            axis.text.x = element_blank(),
-            panel.border = element_blank(),
-            axis.ticks = element_blank(),
-            plot.title = element_text(size = 12, face = "bold"))
-  }
+  
   output$resultado <- renderPlot({
     plot_resultado_materia()
   })
@@ -180,3 +136,29 @@ servidor <- function(input, output, session) {
 }
 
 shinyApp(ui = iu, server = servidor)
+
+plot_resultado_materia <- function() {
+  
+}
+camadas@data %>% 
+  filter(ano == 2020) %>% 
+  filter(materia ==  "MPV-897-2019-1") %>% 
+  slice(1:2) %>% 
+  ggplot(aes(x = "", y = total, fill = Votos)) +
+  geom_bar(colour = "black", stat = "identity", width = 1) +
+  coord_polar("y", start = 0) +
+  ggtitle(ifelse(camadas@data$total[camadas@data$Votos == "Sim"][1] > camadas@data$total[camadas@data$Votos == "Nao"][1],
+                 paste("Resutaldo da votação da ", 
+                       camadas@data$materia[1], "\nAprovada"), 
+                 paste("Resutaldo da votação da ", 
+                       camadas@data$materia[1], "\nReprovada"))) +
+  theme_minimal() + 
+  theme(line = element_blank(),
+        axis.title.x = element_blank(),
+        axis.title.y = element_blank(),
+        axis.text.x = element_blank(),
+        panel.border = element_blank(),
+        axis.ticks = element_blank(),
+        plot.title = element_text(size = 12, 
+                                  face = "bold", 
+                                  hjust = 0.5))
